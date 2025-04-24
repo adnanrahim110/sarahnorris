189b5515a685a87b6360f686f377c02797f66bd0
adnan <adnankaka.0092@gmail.com>
2025-04-24 21:17:28 +0500

    commit

--- src/components/layouts/HowToBeSaved.jsx
+++ src/components/layouts/HowToBeSaved.jsx
@@ -15,7 +15,7 @@ const HowToBeSaved = () => {
             <div className="row items-center justify-between gap-y-5">
               {verses.map((verse, idx) => (
                 <div key={idx} className="md:w-4/12">
-                  <div className="bg-primary-50/10 border md:mx-3.5 border-primary pt-5 pb-10 px-6 rounded-[10px] text-center relative">
+                  <div className="bg-primary-50/30 border md:mx-3.5 border-primary pt-5 pb-10 px-6 rounded-[10px] text-center relative">
                     <h3 className="text-3xl text-secondary">{verse.title}</h3>
                     <span className="absolute text-black/60 right-2.5 text-xs bottom-[5px]">
                       {verse.ref}
@@ -28,7 +28,7 @@ const HowToBeSaved = () => {
         </div>
         <div className="row justify-between items-center mt-16 gap-y-5">
           <div className="lg:w-7/12">
-            <div className="w-full bg-primary-50/10 p-4 rounded-[10px] border border-secondary-300 max-md:text-center">
+            <div className="w-full bg-primary-50/30 p-4 rounded-[10px] border border-secondary-300 max-md:text-center">
               <h2 className="title max-md:mb-4">Christian Resources</h2>
               <p>Check out our recommended Christian books and resources:</p>
             </div>
@@ -39,7 +39,7 @@ const HowToBeSaved = () => {
                 <div key={idx} className="w-full max-md:text-center">
                   <a
                     href={resource.url}
-                    className="block w-full bg-primary-50/10 border border-secondary-300 p-5 rounded-[10px]"
+                    className="block w-full bg-primary-50/30 border border-secondary-300 p-5 rounded-[10px]"
                   >
                     <h4 className="text-xl leading-[0]">{resource.title}</h4>
                   </a>
